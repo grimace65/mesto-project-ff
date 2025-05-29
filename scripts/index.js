dcf4d1247854f4df8d04c97593f45386c6db0b15
Amina Sadykova <amina0916@mail.ru>
2025-05-29 19:15:19 +0300

    Написан первичный код

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
+
+initialCards.forEach(function(cardNode) {
+    const cardItem = createCard(cardNode, removeCard);
+    cardsList.append(cardItem);
+})
+
+function removeCard(item) {
+    item.remove();
+}
+
+function createCard(card, deleteCard) {
+    const cardElement = cardTemplate.cloneNode(true).firstElementChild;
+    const cardTitle = cardElement.querySelector('.card__title');
+    const image = cardElement.querySelector('.card__image');
+
+    cardTitle.textContent = card.name;
+    image.src = card.link;
+    image.alt = card.name;
+
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', function() {
+        deleteCard(cardElement);
+    });
+    return cardElement;
+}
